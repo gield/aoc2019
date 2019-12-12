a7c9485092a78e0d18b351722da2ba780fc2150a
Giel Dops <gieldops@hotmail.com>
2019-12-12 10:50:22 +0100

    More correct solution of day 12

--- day12/solution1.py
+++ day12/solution1.py
@@ -7,13 +7,14 @@ moons = [
 
 for step in range(1000):
     for i in range(len(moons)):
-        for j in range(len(moons)):
+        for j in range(i + 1, len(moons)):
             for axis in range(3):
                 if moons[j]["pos"][axis] < moons[i]["pos"][axis]:
                     moons[i]["vel"][axis] -= 1
+                    moons[j]["vel"][axis] += 1
                 if moons[j]["pos"][axis] > moons[i]["pos"][axis]:
                     moons[i]["vel"][axis] += 1
-
+                    moons[j]["vel"][axis] -= 1
     for i in range(len(moons)):
         for axis in range(3):
             moons[i]["pos"][axis] += moons[i]["vel"][axis]
--- day12/solution2.py
+++ day12/solution2.py
@@ -6,10 +6,6 @@ def lcm(a: int, b: int) -> int:
     return abs(a * b) // math.gcd(a, b)
 
 
-def lcm3(a: int, b: int, c: int) -> int:
-    return lcm(lcm(a, b), c)
-
-
 moons = [
     {"pos": [5, 4, 4], "vel": [0, 0, 0]},
     {"pos": [-11, -11, -3], "vel": [0, 0, 0]},
@@ -17,19 +13,26 @@ moons = [
     {"pos": [-13, 2, 10], "vel": [0, 0, 0]},
 ]
 
-per_axis = []
+steps_per_period = []
 for axis in range(3):
+    positions = [m["pos"][axis] for m in moons]
+    velocities = [m["vel"][axis] for m in moons]
+    initial_positions = positions.copy()
+    initial_velocities = velocities.copy()
     for step in itertools.count():
         for i in range(len(moons)):
-            for j in range(len(moons)):
-                if moons[i]["pos"][axis] > moons[j]["pos"][axis]:
-                    moons[i]["vel"][axis] -= 1
-                if moons[i]["pos"][axis] < moons[j]["pos"][axis]:
-                    moons[i]["vel"][axis] += 1
+            for j in range(i + 1, len(moons)):
+                if positions[i] > positions[j]:
+                    velocities[i] -= 1
+                    velocities[j] += 1
+                if positions[i] < positions[j]:
+                    velocities[i] += 1
+                    velocities[j] -= 1
         for i in range(len(moons)):
-            moons[i]["pos"][axis] += moons[i]["vel"][axis]
-        if all(moons[i]["vel"][axis] == 0 for i in range(len(moons))):
-            per_axis.append(step + 1)
+            positions[i] += velocities[i]
+        if positions == initial_positions and velocities == initial_velocities:
+            steps_per_period.append(step + 1)
             break
 
-print(lcm3(*per_axis) * 2)
+steps_axis1, steps_axis2, steps_axis3 = steps_per_period
+print(lcm(steps_axis1, lcm(steps_axis2, steps_axis3)))
