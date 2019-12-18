8e7bfd6fd0067d70ff9547020a59a093560fe140
Giel Dops <gieldops@hotmail.com>
2019-12-18 18:30:17 +0100

    Day 18 part 1: improve DFS solution

--- day18/solution1.py
+++ day18/solution1.py
@@ -1,76 +1,60 @@
 import numpy as np
+from collections import defaultdict
 from heapq import heapify, heappop, heappush
 
 
-def is_walkable(char: str) -> bool:
-    return char == "." or char == "@" or (char.isalpha() and char.islower())
-
-
-memo = {}
-def get_shortest_path(vault, door_locations, p1, p2):
-    hashed_vault = ("".join(sorted(door_locations.keys())), p1, p2)
-    if hashed_vault in memo:
-        return memo[hashed_vault]
-    visited = {p1}
-    todo = [(0, p1, [])]
+def get_possible_routes(vault, p_source):
+    # From some position (i.e. a key or the entrance), how much does it cost to
+    # get to every key? Also include all points of interest (i.e. keys or
+    # doors) encountered on that route as interesting information.
+    possible_routes = {}
+    visited = {p_source}
+    todo = [(0, p_source, "")]
     heapify(todo)
     while todo:
-        cost, p_old, path_old = heappop(todo)
+        cost, p_old, points_of_interest = heappop(todo)
+        if vault[p_old] not in "#.@" and cost > 0:
+            possible_routes[vault[p_old]] = (cost, points_of_interest)
+            points_of_interest += vault[p_old]
         for x_diff, y_diff in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
             p_new = p_old[0] + x_diff, p_old[1] + y_diff
-            path_new = path_old + [p_new]
-            if p_new == p2:
-                memo[hashed_vault] = path_new
-                return path_new
-            if p_new not in visited and is_walkable(vault[p_new]):
+            if p_new not in visited and vault[p_new] not in "#":
                 visited.add(p_new)
-                heappush(todo, (cost + 1, p_new, path_new))
-    memo[hashed_vault] = []
-    return []
+                heappush(todo, (cost + 1, p_new, points_of_interest))
+    return possible_routes
 
 
-min_score = float('inf')
-def dfs(vault, key_locations, door_locations, p_player, num_steps, unlocked_keys):
+def get_all_routes(vault):
+    # For all interesting positions (i.e. keys and the entrance), get all
+    # possible routes.
+    all_routes = {}
+    for y in range(vault.shape[1]):
+        for x in range(vault.shape[0]):
+            if vault[x, y] not in "#.":
+                all_routes[vault[x, y]] = get_possible_routes(vault, (x, y))
+    return all_routes
+
+
+def dfs(location, num_steps, unlocked_keys):
     global min_score
     if num_steps > min_score:
         return float('inf')
-    if not key_locations:  # We have a solution
+    if len(unlocked_keys) == len(all_keys):  # We have a solution
         if num_steps < min_score:  # We have a better solution
             min_score = num_steps
         return num_steps
 
-    reachable = [(get_shortest_path(vault, door_locations, p_player, p), p)
-                for p in key_locations.values()]
-    reachable = [(path, p) for path, p in reachable if len(path)]
-    reachable.sort(key=lambda t: len(t[0]))
-    if len(reachable) == 0:
-        return float('inf')
-    elif len(reachable) > 1:
-        for path1, p1 in reachable:
-            for path2, p2 in reachable:
-                if p1 == p2:
-                    continue
-                if p1 in path2:
-                    del reachable[reachable.index((path2, p2))]
     scores = []
-    for path, p_key in reachable:
-        if num_steps + len(path) >= min_score:
+    for key in all_keys - unlocked_keys:
+        cost, points_of_interest = all_routes[location][key]
+        if num_steps + cost >= min_score:
             continue
-        temp_vault = vault.copy()
-        temp_key_locations = key_locations.copy()
-        temp_door_locations = door_locations.copy()
-
-        key = temp_vault[p_key]
-        temp_vault[p_key] = "."
-        del temp_key_locations[key]
-        if key.upper() in temp_door_locations:
-            p_door = temp_door_locations.pop(key.upper())
-            temp_vault[p_door] = "."
-        temp_vault[p_player] = "."
-        temp_vault[p_key] = "@"
-        score = dfs(temp_vault, temp_key_locations, temp_door_locations,
-                    p_key, num_steps + len(path), unlocked_keys + [key])
-        scores.append(score)
+        is_reachable = all(k in unlocked_keys or k.lower() in unlocked_keys
+                           for k in points_of_interest)
+        if is_reachable:
+            temp_unlocked_keys = unlocked_keys | {key}
+            score = dfs(key, num_steps + cost, temp_unlocked_keys)
+            scores.append(score)
     return min(scores) if scores else float('inf')
 
 
@@ -79,17 +63,10 @@ with open("input.txt", "r") as f:
 
 max_y, max_x = len(raw_vault), len(raw_vault[0])
 vault = np.array([list(line) for line in raw_vault]).T
-key_locations = {}
-door_locations = {}
-for y in range(vault.shape[1]):
-    for x in range(vault.shape[0]):
-        char = vault[x, y]
-        if char == "@":
-            p_player = x, y
-        elif char.isalpha():
-            if char.islower():
-                key_locations[char] = x, y
-            else:
-                door_locations[char] = x, y
+all_keys = {vault[x, y]
+            for y in range(vault.shape[1]) for x in range(vault.shape[0])
+            if vault[x, y].islower()}
+all_routes = get_all_routes(vault)
 
-print(dfs(vault, key_locations, door_locations, p_player, 0, []))
+min_score = float('inf')
+print(dfs("@", 0, set()))
